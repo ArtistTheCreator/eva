5e0840ca250e57fdfecbad45d40daadc901fe472
Rostislav <rostislav@MacBook-Pro-Rostislav.local>
2023-11-07 19:37:45 +0300

    Updates

--- assets/index-9ae02fa8.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-c156aaa6.js";import{R as o}from"./index-bb2d7e0d.js";import"./CloseCircleFilled-4592d4ec.js";const u=()=>t.jsx(o,{status:"404",title:"Page not found."});export{u as default};
